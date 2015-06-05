f4834675614a6d894f86f023d03ee16c4faac51b
Gongpei <ggongpei@qq.com>
2015-06-05 16:24:01 +0800

    add localstorage to store color history

--- main.js
+++ main.js
@@ -4,37 +4,55 @@
 	var rgb  = doc.getElementById('rgb');
 	var sub  = doc.getElementById('submit');
 	var body = doc.getElementsByTagName('body')[0];
-
+	
+	body.onload        = init;
 	sub.onclick        = getResult;
 	document.onkeydown = keyDownSearch;
 
-	var colorList = [];
-	if(window.localStorage) {
-		var local = window.localStorage;
-		for(var i = 0; i < local.length; i++) {
-			var localHex = local.key(i);
-			if(!/hex\(\w+\)/.test(localHex)) {
-				continue;
+	function init() {
+
+		var colorList = [];
+		if(window.localStorage) {
+			var local = window.localStorage;
+			for(var i = 0; i < local.length; i++) {
+				var localHex = local.key(i);
+				if(!/hex\(\w+\)/.test(localHex)) {
+					continue;
+				}
+				var localrgb = local.getItem(localHex);
+
+				console.log(localHex + '/' + localrgb + "\n");
+				colorList.push(localHex + '/' + localrgb);
 			}
-			var localrgb = local.getItem(localHex);
-
-			console.log(localHex + '/' + localrgb + "\n");
-			colorList.push(localHex + '/' + localrgb);
 		}
-	}
 
-	colorHtml = '<div id="colorList" style="position: absolute;left:0;bottom:0;overflow: hidden;">';
-	for(var i = 0; i < colorList.length; i++) {
-		var colorHex = colorList[i].split('/')[0].replace("hex(", "#").replace(')', "");
-		var colorRgb = colorList[i].split('/')[1];
-		
-		colorHtml += '<span style="display:inline-block;height:20px;width:20px;background:' + colorHex + '"></span>';
+		var windowWidth = document.body.clientWidth;
+		var colorHtml   = '';
+		var list = doc.getElementById('colorList');
+		for(var i = 0; i < colorList.length; i++) {
+			var colorHex = colorList[i].split('/')[0].replace("hex(", "#").replace(')', "");
+			var colorRgb = colorList[i].split('/')[1];
+			
+			colorHtml += '<span class="item" style="width:' + windowWidth/colorList.length + 'px;background:' + colorHex + '"></span>';
 
-		console.log(colorHex + colorRgb);
-	}
-	colorHtml += '</div>';
+			console.log(colorHex + colorRgb);
+		}
+		list.innerHTML = colorHtml;
 
-	body.innerHTML += colorHtml;
+		var lists = doc.getElementsByTagName('span');
+		for (var i = lists.length - 1; i >= 0; i--) {
+			if(lists[i].className == 'item') {
+				console.log(lists[i].style.background);
+				lists[i].onclick = function() {
+					body.style.background = this.style.background;
+					rgb.value = this.style.background;
+					hexTemp   = rgb2hex(this.style.background);
+					hex.value = '#' + hexTemp[0] + hexTemp[1] + hexTemp[2];
+				};
+			}
+		};
+		/*body.innerHTML += colorHtml;*/
+	};
 
    	function getResult() {
 
@@ -54,19 +72,7 @@
 				hexVal    = hexVal.replace(/#/,'');
 				rgb.value = '';
 
-				switch(hexVal.length) {
-					case 3: result = parseInt(hexVal[0] + '' + hexVal[0], 16) + ',' +
-					                 parseInt(hexVal[1] + '' + hexVal[1], 16) + ',' +
-					                 parseInt(hexVal[2] + '' + hexVal[2], 16); 
-					        break;
-					case 6: result = parseInt(hexVal[0] + '' + hexVal[1], 16) + ',' +
-					                 parseInt(hexVal[2] + '' + hexVal[3], 16) + ',' +
-					                 parseInt(hexVal[4] + '' + hexVal[5], 16); 
-					        break;
-					default: alert('Wrong Format'); return;
-				}
-
-				result    = 'rgb(' + result + ')'; 
+				result    = 'rgb(' + hex2rgb(hexVal) + ')'; 
 				rgb.value = result;
 
 				if(window.localStorage) {
@@ -77,19 +83,17 @@
 			} else if(rgbVal && rgbVal.match(/[Rr][Gg][Bb]\(\d+,\d+,\d+\)/)){
 
 				temp      = rgbVal;
-				rgbVal    = rgbVal.replace(/rgb/, '').replace(/\(/, '').replace(/\)/, '').split(',');
 				hex.value = '';
 
-				rgbVal[0] = parseInt(rgbVal[0], 10).toString(16).length == 2 ? parseInt(rgbVal[0], 10).toString(16) : '0' + parseInt(rgbVal[0], 10).toString(16);
-				rgbVal[1] = parseInt(rgbVal[1], 10).toString(16).length == 2 ? parseInt(rgbVal[1], 10).toString(16) : '0' + parseInt(rgbVal[1], 10).toString(16);
-				rgbVal[2] = parseInt(rgbVal[2], 10).toString(16).length == 2 ? parseInt(rgbVal[2], 10).toString(16) : '0' + parseInt(rgbVal[2], 10).toString(16);
+				rgbVal = rgb2hex(rgbVal);
 
 				result    = '#' + rgbVal[0] + rgbVal[1] + rgbVal[2];
 				hex.value = result;
 
+				console.log(result + temp);
 				if(window.localStorage) {
 					var local = window.localStorage;
-					local.setItem('hex(' + result + ')', temp);
+					local.setItem('hex(' + result.replace('#', '') + ')', temp);
 				}
 				
 			} else {
@@ -106,7 +110,33 @@
 		//console.log(hexVal + '-' + rgbVal + '-'  + result);
 		body.style.background = result;
 
-	};
+	}
+
+	function hex2rgb(hexVal) {
+		var result = '';
+		switch(hexVal.length) {
+			case 3: result = parseInt(hexVal[0] + '' + hexVal[0], 16) + ',' +
+			                 parseInt(hexVal[1] + '' + hexVal[1], 16) + ',' +
+			                 parseInt(hexVal[2] + '' + hexVal[2], 16); 
+			        break;
+			case 6: result = parseInt(hexVal[0] + '' + hexVal[1], 16) + ',' +
+			                 parseInt(hexVal[2] + '' + hexVal[3], 16) + ',' +
+			                 parseInt(hexVal[4] + '' + hexVal[5], 16); 
+			        break;
+			default: alert('Wrong Format'); return;
+		}
+		return result;
+	}
+
+	function rgb2hex(rgbVal) {
+
+		rgbVal    = rgbVal.replace(/rgb/, '').replace(/\(/, '').replace(/\)/, '').split(',');
+		rgbVal[0] = parseInt(rgbVal[0], 10).toString(16).length == 2 ? parseInt(rgbVal[0], 10).toString(16) : '0' + parseInt(rgbVal[0], 10).toString(16);
+		rgbVal[1] = parseInt(rgbVal[1], 10).toString(16).length == 2 ? parseInt(rgbVal[1], 10).toString(16) : '0' + parseInt(rgbVal[1], 10).toString(16);
+		rgbVal[2] = parseInt(rgbVal[2], 10).toString(16).length == 2 ? parseInt(rgbVal[2], 10).toString(16) : '0' + parseInt(rgbVal[2], 10).toString(16);
+
+		return rgbVal;
+	}
 
 	function keyDownSearch(e) {    
         // 兼容FF和IE和Opera    
